21635923554bad3ab7cca0cdc23e6e7f0d76db02
uzi-belfayez <rayenbelfayez@gmail.com>
2025-12-22 13:09:14 +0100

    nikzokomek

--- ir_dec_risb.vhd
+++ ir_dec_risb.vhd
@@ -7,72 +7,112 @@ use work.constants.all;
 entity ir_dec_risb is
     generic (
         dataWidth   :   integer:=32;
-        aluOpWidth  :   integer:=5  -- Le signal de sortie fait 5 bits
+        aluOpWidth  :   integer:=5 
     );
     port ( 
         instr           :   in    std_logic_vector (dataWidth - 1 downto 0);
-        
+        bres            :   in    std_logic;
+        btype           :   out   std_logic_vector (2 downto 0); 
         aluOp           :   out   std_logic_vector (aluOpWidth - 1 downto 0);
         insType         :   out   std_logic_vector(2 downto 0);
-        
-        RI_sel          :   out   std_logic; 
-        rdWrite         :   out   std_logic; 
-        wrMem           :   out   std_logic; 
-        loadAcc         :   out   std_logic; 
-        
-        memType         :   out   std_logic_vector(2 downto 0);
-        pc_load, bsel   :   out   std_logic
+        loadType        :   out   std_logic_vector(2 downto 0);
+        immSig          :   out   std_logic;
+        writeDataMem    :   out   std_logic;
+        memDataOutSig   :   out   std_logic;
+        rdWrite         :   out   std_logic;
+        bsel            :   out   std_logic;
+        pc_load         :   out   std_logic;
+        clk             :   in    std_logic;
+        reset           :   in    std_logic
     );
 end entity ir_dec_risb;
 
 architecture behav of ir_dec_risb is
-    alias opcode : std_logic_vector(6 downto 0) is instr(6 downto 0);
-    alias funct3 : std_logic_vector(2 downto 0) is instr(14 downto 12);
-    alias funct7_b5 : std_logic is instr(30);
+    alias funct3    :   std_logic_vector(2 downto 0) is instr(14 downto 12);
+    alias opcode    :   std_logic_vector(6 downto 0) is instr(6 downto 0);
+    signal instType_sig : std_logic_vector(2 downto 0); -- Signal interne pour lecture
 begin
-    process(instr, opcode, funct3, funct7_b5)
+
+    -- Process de Décodage de Type
+    process (opcode)
     begin
-        -- Initialisation par défaut
-        rdWrite <= '0'; RI_sel <= '0'; wrMem <= '0';
-        loadAcc <= '0';
-        insType <= UNKTYP; 
-        aluOp   <= (others => '0'); -- Initialise les 5 bits à 0
-        memType <= "010"; 
-        pc_load <= '0'; bsel <= '0';
-
         case opcode is
-            when R_TYPE_OPCODE => 
-                insType <= R_TYPE; rdWrite <= '1'; RI_sel <= '0'; 
-                -- Correction : On ajoute un '0' devant pour faire 5 bits
-                aluOp <= '0' & funct7_b5 & funct3; 
-
-            when I_TYPE_OPCODE => 
-                insType <= I_TYPE; rdWrite <= '1'; RI_sel <= '1';
-                if funct3 = "101" then 
-                    -- SRAI/SRLI : Besoin du bit 30
-                    aluOp <= '0' & funct7_b5 & funct3; 
-                else 
-                    -- ADDI, SLTI, etc : Bit 30 forcé à 0
-                    aluOp <= "00" & funct3; 
-                end if;
-
-            when L_TYPE_OPCODE => 
-                insType <= L_TYPE; 
-                rdWrite <= '1'; 
-                RI_sel <= '1'; 
-                loadAcc <= '1';    
-                wrMem <= '0'; 
-                memType <= funct3; 
-                aluOp   <= (others => '0'); -- 5 bits à 0 (ADD pour calcul adresse)
-
-            when S_TYPE_OPCODE => 
-                insType <= S_TYPE; rdWrite <= '0'; RI_sel  <= '1'; 
-                loadAcc <= '0'; 
-                wrMem   <= '1'; 
-                memType <= funct3; 
-                aluOp   <= (others => '0'); -- 5 bits à 0 (ADD pour calcul adresse)
-
-            when others => null;
+            when R_TYPE_OPCODE    => instType_sig  <=  R_TYPE;
+            when I_TYPE_OPCODE    => instType_sig  <=  I_TYPE;
+            when S_TYPE_OPCODE    => instType_sig  <=  S_TYPE;
+            when B_TYPE_OPCODE    => instType_sig  <=  B_TYPE;
+            when U_TYP1_OPCODE    => instType_sig  <=  U_TYPE;
+            when U_TYP2_OPCODE    => instType_sig  <=  U_TYPE;
+            when J_TYP1_OPCODE    => instType_sig  <=  J_TYPE;
+            when J_TYP2_OPCODE    => instType_sig  <=  J_TYPE;
+            when L_TYPE_OPCODE    => instType_sig  <=  L_TYPE;
+            when others           => instType_sig  <=  UNKTYP;
         end case;
     end process;
+    insType <= instType_sig;
+
+    -- Process ALU Control (C'est ICI qu'était l'erreur)
+    process (instType_sig, funct3, instr)
+    begin
+        -- Initialisation propre (5 bits à 0)
+        aluOp <= (others => '0'); 
+
+        case instType_sig is
+            when R_TYPE  => 
+                -- CORRECTION : Ajout du '0' pour faire 5 bits (1 + 1 + 3)
+                aluOp   <=  '0' & instr(30) & funct3;
+                
+            when I_TYPE  =>  
+                aluOp(2 downto 0)   <=  funct3;
+                aluOp(4)            <=  '0'; -- CORRECTION : On force le bit 4 à 0
+                
+                if funct3 = "101" then -- SRAI / SRLI
+                    aluOp(3)    <= instr(30);
+                else
+                    aluOp(3)    <= '0';
+                end if;
+                
+            when others =>  
+                -- CORRECTION : "00000" (5 bits) et non "0000" (4 bits)
+                aluOp   <=  "00000"; 
+        end case;
+    end process;
+
+    -- Process Control Signals
+    process (instType_sig, funct3, bres)
+    begin
+        memDataOutSig   <=  '0';
+        loadType        <=  "010"; 
+        writeDataMem    <=  '0';
+        rdWrite         <=  '0';
+        bsel            <=  '0';
+        pc_load         <=  '0';
+        immSig          <=  '0'; -- Default
+
+        case instType_sig is
+            when R_TYPE  =>   
+                immSig  <=  '0';
+                rdWrite <=  '1';
+            when I_TYPE  =>   
+                immSig  <=  '1';
+                rdWrite <=  '1';
+            when L_TYPE  =>   
+                immSig  <=  '1';
+                memDataOutSig   <=  '1';
+                loadType    <=  funct3;
+                rdWrite <=  '1';
+            when S_TYPE  =>   
+                immSig  <=  '1'; -- Pour calculer l'adresse (rs1 + imm)
+                loadType    <=  funct3; -- Pour le module SM
+                writeDataMem    <= '1'; -- Active l'écriture
+            when B_TYPE =>
+                bsel    <= bres;
+                pc_load <= bres;
+            when others  =>   
+                immSig  <=  '0'; 
+        end case;
+    end process;
+
+    btype   <=  funct3;
+
 end behav;
